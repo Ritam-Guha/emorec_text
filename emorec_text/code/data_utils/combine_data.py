4e34d430b79044dd4655541674ee13336f496fc3
ritz <ritamguha16@gmail.com>
2022-10-30 19:30:10 -0400

    added ground truth formation and initial model text2emotion

---
+++ emorec_text/code/data_utils/combine_data.py
@@ -0,0 +1,56 @@
+import emorec_text.config as config
+from emorec_text.code.utils.path_utils import create_dir
+
+import pandas as pd
+import os
+import copy
+
+
+def combine_data():
+    create_dir("data/text_emotion")
+    list_video_emotions = os.listdir(f"{config.BASE_PATH}/data/video_emotions")
+    list_transcriptions = os.listdir(f"{config.BASE_PATH}/data/transcription_storage")
+    list_common_videos = list(set(list_transcriptions).intersection(set(list_video_emotions)))
+
+    for video_id in list_common_videos:
+        try:
+            df_video_emotions = pd.read_csv(f"{config.BASE_PATH}/data/video_emotions/{video_id}")
+            df_transcription = pd.read_csv(f"{config.BASE_PATH}/data/transcription_storage/{video_id}")
+
+            # get the emotion
+            df_video_emotions = df_video_emotions[["frame", "dominant_emotion"]]
+            df_video_emotions["frame"] = df_video_emotions["frame"].str.extract('(\d+)').astype(int)
+            df_video_emotions = df_video_emotions.sort_values(by="frame")
+
+            # majority voting in emotions
+            df_emotion = copy.deepcopy(df_transcription)
+            df_emotion["end"] = df_emotion["start"] + df_emotion["duration"]
+
+            start_time = list(df_emotion["start"])
+            end_time = list(df_emotion["end"])
+            list_dominant_emotions = []
+
+            for (start, end) in zip(start_time, end_time):
+                cur_start_time = int(start)
+                cur_end_time = int(end)
+                list_cur_dominant_emotions = list(df_video_emotions[df_video_emotions["frame"].between(cur_start_time, cur_end_time)]["dominant_emotion"])
+
+                try:
+                    list_dominant_emotions.append(max(set(list_cur_dominant_emotions), key = list_dominant_emotions.count))
+                except:
+                    list_dominant_emotions.append("NA")
+
+            df_emotion["dominant_emotion"] = list_dominant_emotions
+            df_emotion.to_csv(f"{config.BASE_PATH}/data/text_emotion/{video_id}", index=False)
+            print(f"{video_id} done...")
+
+        except:
+            continue
+
+def main():
+    combine_data()
+
+
+if __name__ == "__main__":
+    main()
+
--- emorec_text/code/data_utils/data_loader.py
+++ emorec_text/code/data_utils/data_loader.py
@@ -1,8 +1,60 @@
-import os
 import emorec_text.config as config
+
+import os
+import numpy as np
+import pandas as pd
 
 
 class DataLoader:
     def __init__(self,
-                 type_data="videos"):
-        self.data_path = f"{config.BASE_PATH}/data/{type_data}"
+                 seed=0,
+                 train_percent=0.7):
+
+        self.data_path = f"{config.BASE_PATH}/data/text_emotion"
+        self.train_percent = train_percent
+        self.seed = seed
+        self.data = self.read_data()
+
+    def read_data(self):
+        list_files = os.listdir(self.data_path)
+        idx_list = np.arange(len(list_files))
+        train_size = int(self.train_percent * len(list_files))
+
+        np.random.seed(self.seed)
+        np.random.shuffle(idx_list)
+
+        train_idx = idx_list[:train_size]
+        test_idx = idx_list[train_size:]
+
+        train_list = [list_files[i] for i in train_idx]
+        test_list = [list_files[i] for i in test_idx]
+
+        data = {"train": {"text": [], "emotion": []},
+                "test": {"text": [], "emotion": []}}
+
+        # train
+        for video_id in train_list:
+            df_emotion = pd.read_csv(f"{config.BASE_PATH}/data/text_emotion/{video_id}")
+            data["train"]["text"].append(list(df_emotion["text"]))
+            data["train"]["emotion"].append(list(df_emotion["dominant_emotion"]))
+
+        # test
+        for video_id in test_list:
+            df_emotion = pd.read_csv(f"{config.BASE_PATH}/data/text_emotion/{video_id}")
+            data["test"]["text"].append(list(df_emotion["text"]))
+            data["test"]["emotion"].append(list(df_emotion["dominant_emotion"]))
+
+        print(f"train size: {len(train_list)}")
+        print(f"test size: {len(test_list)}")
+        return data
+
+    def get_data(self):
+        return self.data
+
+
+def main():
+    data_loader = DataLoader()
+
+
+if __name__ == "__main__":
+    main()
