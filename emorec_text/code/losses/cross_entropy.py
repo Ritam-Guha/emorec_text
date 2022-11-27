a2dc9c617c7150d7ee3d20ade36f0a81882e8e63
evali0421 <evali0421@gmail.com>
2022-11-26 22:50:32 -0500

    add line to convert probabilities

--- emorec_text/code/losses/cross_entropy.py
+++ emorec_text/code/losses/cross_entropy.py
@@ -1,5 +1,23 @@
 import torch
+import numpy as np
 
+def get_ground_truth_indices(target):
+    """
+    Return a 1-D tensor object that contains the indices of
+    ground truth lables, which is the input format assumed by
+    `torch.nn.CrossEntropyLoss(..)`
+    Input:
+    `target` is the tensor vector that contains ground truth labels.
+    Example:
+    target = tensor([[1., 0., 0., 0., 0.],
+                    [0., 1., 0., 0., 0.],
+                    [0., 0., 1., 0., 0.]])
+    Output:
+    tensor([0, 1, 2])
+    """
+    np_target = target.numpy(force=True)
+    ground_truth_indices = torch.from_numpy(np.where(np_target == 1)[1])
+    return ground_truth_indices
 
 def cross_entropy_loss_function(ground_truth,
                                 predictions):
@@ -10,12 +28,16 @@ def cross_entropy_loss_function(ground_truth,
     Output of this function:
     tensor(5.2485, grad_fn=<NllLossBackward0>)
     """
+    ground_truth_indices = get_ground_truth_indices(ground_truth)
+
+    predictions = torch.nn.Softmax(dim=-1)(predictions)
 
     # taking log b/c torch.nn.CrossEntropyLoss() assumes
     # logit input
     predictions = torch.log(predictions)
 
     loss = torch.nn.CrossEntropyLoss(reduction='mean')
-    output = loss(predictions, ground_truth)
+    output = loss(predictions, ground_truth_indices)
+    # print(output.requires_grad)
 
     return output
